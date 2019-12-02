8c87a9e9ac64b728c4ae58cb9efde0d48feea624
{xXgit-wrekt-skrubXx} <leulzerihun2001@gmail.com>
2019-12-01 22:53:39 -0800

    first version

--- javascript canvas.js
+++ javascript canvas.js
@@ -5,7 +5,7 @@ canvas.height = window.innerHeight;
 var c= canvas.getContext(`2d`);
 console.log(canvas)
 
-c.fillRect(0,0,1000,1000)
+c.fillRect(0,0,,1000)
 c.beginPath();
 c.moveTo(100,50);
 c.lineTo(100,900);
