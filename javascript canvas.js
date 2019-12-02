a9d725508097bc4fdac2d502fbad5439b2a04e63
xXgit-wrekt-skrubXx <leulzerihun2001@gmail.com>
2019-12-01 22:35:24 -0800

    Showcase project game canvas

---
+++ javascript canvas.js
@@ -0,0 +1,13 @@
+var canvas = document.querySelector(`canvas`)
+;
+canvas.width= window.innerWidth;
+canvas.height = window.innerHeight;
+var c= canvas.getContext(`2d`);
+console.log(canvas)
+
+c.fillRect(0,0,1000,1000)
+c.beginPath();
+c.moveTo(100,50);
+c.lineTo(100,900);
+c.lineTo(400,300)
+c.strokeStyle= "#fa34a";
