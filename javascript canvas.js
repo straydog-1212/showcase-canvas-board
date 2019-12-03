998c35b722163fa49a5c044dbc94699d30f49abe
TIGABU LEUL <702022755@cart.ads>
2019-12-03 15:32:03 -0800

    canvas version 1.6

--- javascript canvas.js
+++ javascript canvas.js
@@ -4,26 +4,10 @@ canvas.height = window.innerHeight - 50;
 var c= canvas.getContext(`2d`);
 console.log(canvas)
 
-/*c.fillRect(1300,695,300,300) //bottom squares
-c.fillRect(1700,695,300,300)
-
-c.fillRect(1300,1095,300,300) // middle squares
-c.fillRect(1700,695,300,300)
-
-c.fillRect(1300,695,300,300) // top squares
-c.fillRect(1700,695,300,300)
-
-c.beginPath();
-c.moveTo(136,0);
-//for (i= 0; i< 10; i )
-c.lineTo(544,900);
-c.lineTo(400,300);
-c.strokeStyle = "#fa34a3"
-c.stroke()*/
-
 c.stroke() 
-c.fillStyle = `rgba(225,0,0,0.5)`
-var squareSize = 300
+
+
+var squareSize = 100
 height = 1042
 width = 2048
 var rectXPos = 50;
@@ -36,14 +20,9 @@ for (let y = 0; y <= height; y += squareSize){
         c.fillRect(x,y,squareSize,squareSize)
         c.fillStyle = `rgba(46,192,225,1)`
         c.fillRect(x,y,squareSize- 1,squareSize- 1)
+        mouseClickMethod(didItDit)
     }
 }
 
-
-function drawBorder(xPos, yPos, width, height, thickness = 1)
-{
-  ctx.fillStyle='#000';
-  ctx.fillRect(xPos - (thickness), yPos - (thickness), width + (thickness * 2), height + (thickness * 2));
-}
 console.log(window.innerWidth)
 console.log(window.innerHeight)
