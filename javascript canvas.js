09718f3aba3cf0eb2ee2b14cd4f857e3c24eb83f
TIGABU LEUL <702022755@cart.ads>
2019-12-03 14:21:00 -0800

    canvas 1.4

--- javascript canvas.js
+++ javascript canvas.js
@@ -26,11 +26,22 @@ c.fillStyle = `rgba(225,0,0,0.5)`
 var squareSize = 300
 height = 1042
 width = 2048
+var rectXPos = 50;
+var rectYPos = 50;
+
+
 for (let y = 0; y <= height; y += squareSize){
     for (let x=0; x<= width; x+= squareSize) {
         c.fillRect(x,y,squareSize,squareSize)
         c.border
     }
+}
+
+
+function drawBorder(xPos, yPos, width, height, thickness = 1)
+{
+  ctx.fillStyle='#000';
+  ctx.fillRect(xPos - (thickness), yPos - (thickness), width + (thickness * 2), height + (thickness * 2));
 }
 console.log(window.innerWidth)
 console.log(window.innerHeight)
