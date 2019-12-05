ebfccbe54e5c8bf13af08c8bfb5a338843d984f4
TIGABU LEUL <702022755@cart.ads>
2019-12-05 15:30:47 -0800

    showcase canvas1.9

--- javascript canvas.js
+++ javascript canvas.js
@@ -13,14 +13,15 @@ width = 600
 var rectXPos = 50;
 
 c.fillRectectYPos = 50;
-
-for (let y = 0; y <= height; y += squareSize){
-    for (let x = 0; x <= width; x += squareSize) {
-        c.fillStyle = `rgba(0,0,0,1)`
-        c.fillRect(x,y,squareSize,squareSize)
-        c.fillStyle = `rgba(46,192,225,1)`
-        c.fillRect(x,y,squareSize- 1,squareSize- 1)
-        //mouseClickMethod(didItDit)
+function createGrid(){
+    for (let y = 0; y <= height; y += squareSize){
+        for (let x = 0; x <= width; x += squareSize) {
+            c.fillStyle = `rgba(0,0,0,1)`
+            c.fillRect(x,y,squareSize,squareSize)
+            c.fillStyle = `rgba(46,192,225,1)`
+            c.fillRect(x,y,squareSize- 1,squareSize- 1)
+            //mouseClickMethod(didItDit)
+        }
     }
 }
 moveTo(500,500)
@@ -33,16 +34,10 @@ for (let y = 0; y <= height; y += squareSize){
         //mouseClickMethod(didItDit)
     }
 }
-
-console.log(window.innerWidth)
-console.log(window.innerHeight)
-
-
-
-
-
-
-
+button.addEventListener ("click", function() {
+    history.push(choice);
+    showStory();
+})
 
 
 
