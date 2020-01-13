a0d4d26de0bf5e040f8018d96c462dab68ecabdd
SHACKELFORD REESE <150051514@cart.ads>
2020-01-13 13:08:28 -0800

    hit

--- javascript canvas.js
+++ javascript canvas.js
@@ -55,6 +55,7 @@ var grid = clickableGrid(10,10,function(el,row,col,i){
 
 document.body.appendChild(grid);
      
+this.player1 = new player ('eeee')
 
 function clickableGrid( rows, cols, callback ){
     var i=0;
@@ -63,16 +64,16 @@ function clickableGrid( rows, cols, callback ){
 //creates grid
     for (var r=0;r<rows;++r){
         var tr = grid.appendChild(document.createElement('tr'));
- //creates row     
+  
         for (var c=0;c<cols;++c){
             var cell = tr.appendChild(document.createElement('td'));
             cell.innerHTML = ++i;
-//creates column
+
             cell.addEventListener('click',(function(el,r,c,i){
                 return function(){
                     callback(el,r,c,i);
                     player1.Carrier.updatehits
-                    alert('hits')
+                    alert('aaAAAAAAAAAAAH')
 //makes squares clickable
                 }
             })(cell,r,c,i),false);
@@ -89,18 +90,16 @@ function ship (name,hits,originX,originY,orientation){
 
 //players ships
 }
+}
 function player(name){
         this.name = name;
-        this.Carrier = new ship('Carrier',5,0,0,up)
-        this.Battleship = new ship ('Battleship',4,0,0,up)
-        this.Destroyer =new ship ('Destroyer',2,0,0,up)
-        this.Cruiser = new  ship('Cruiser',3,0,0,up)
-        this.Submarine = new ship ('submarine',3,0,0,up)
-    
-    }
-   var player1 = new player ('USA')
+        this.Carrier = new ship('Carrier',5,0,0,'up')
+        this.Battleship = new ship ('Battleship',4,0,0,'up')
+        this.Destroyer =new ship ('Destroyer',2,0,0,'up')
+        this.Cruiser = new  ship('Cruiser',3,0,0,'up')
+        this.Submarine = new ship ('submarine',3,0,0,'up')
+          
     };
+    
 
-var player1= new player 
-    let p1Carrier = new ship ('Carrier',5)
     
